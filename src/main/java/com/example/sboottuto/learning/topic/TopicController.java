2b0190f94e59f40292cdc327e81dab622d2028ce
PJ <goldprajnawi@gmail.com>
2021-06-16 22:13:45 +0800

    crud service with app pro

--- src/main/java/com/example/sboottuto/learning/topic/TopicController.java
+++ src/main/java/com/example/sboottuto/learning/topic/TopicController.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +25,20 @@ public class TopicController {
     @RequestMapping("/topic/{name}")
     public Topic getTopic(@PathVariable String name){
         return topicService.getTopic(name);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void setTopic(@RequestBody Topic topic){
+         topicService.setTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{name}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String name){
+        topicService.updateTopic(topic, name);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{name}")
+    public void deleteTopic(@PathVariable String name){
+        topicService.deleteTopic(name);
     }
 }
--- src/main/java/com/example/sboottuto/learning/topic/TopicService.java
+++ src/main/java/com/example/sboottuto/learning/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.example.sboottuto.learning.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,11 +8,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>( Arrays.asList(
         new Topic(1,"Java","Java Description"),
         new Topic(2,"Javascript","Javascript Description"),
         new Topic(3,"SpringBoot","SpringBoot Description")
-     );
+        ));
 
      public List<Topic>getAllTopics(){
          return topics;
@@ -20,4 +21,22 @@ public class TopicService {
      public Topic getTopic(String name){
          return topics.stream().filter(t-> t.getName().equals(name)).findFirst().get();
      }
+
+    public void setTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String name) {
+        for(int i=0; i<topics.size(); i++){
+            Topic t = topics.get(i);
+            if(t.getName().equals(name)){
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String name) {
+        topics.removeIf(t-> t.getName().equals(name));
+    }
 }
